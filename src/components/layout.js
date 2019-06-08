c7a7e2d0509f6888dc2271f524289742e434bbaa
Ilya Puchka <ilyapuchka@gmail.com>
2019-06-08 21:22:11 +0100

    Initial commit from gatsby: (https://github.com/gatsbyjs/gatsby-starter-blog.git)

---
+++ src/components/layout.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import { rhythm, scale } from "../utils/typography"
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <header>{header}</header>
+        <main>{children}</main>
+        <footer>
+          <p>
+            <a style={{ boxShadow: `none` }} rel={"license"} href={"http://creativecommons.org/licenses/by/4.0/"}><img alt={"Creative Commons Licence"} src={"https://i.creativecommons.org/l/by/4.0/88x31.png"} /></a><br />This work is licensed under a <a rel={"license"} href={"http://creativecommons.org/licenses/by/4.0/"}>Creative Commons Attribution 4.0 International License</a>.
+          </p>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
+    )
+  }
+}
+
+export default Layout
