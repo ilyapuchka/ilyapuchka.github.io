ad45e784aa297ceceec16b29da8a14f7d170ea17
Ilya Puchka <ilyapuchka@gmail.com>
2019-10-28 21:02:11 +0000

    added GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-57638998-1`,
       },
     },
     `gatsby-plugin-feed`,
